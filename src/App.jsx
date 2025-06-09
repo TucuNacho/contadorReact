1927f6dabca2c1e26c08d9b7522e55d91ac2045d
Nacho <barrojojoaquin2@gmail.com>
2025-06-09 20:33:32 -0300

    agregue el maquetado del componente Informacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Informacion from "./components/Informacion";
 
 function App() {
   return (
-    <>
+    <main className="container my-5">
+
       <h1>hola mundo</h1>
       <Informacion/>
-    </>
+    </main>
   );
 }
 export default App;
