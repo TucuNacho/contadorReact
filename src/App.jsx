bd9d780a6f275e4d53bcd775bf5778d28efd6599
Nacho <barrojojoaquin2@gmail.com>
2025-06-09 20:20:45 -0300

    agregue el componente Informacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "./components/Informacion";
+
 function App() {
-  return <h1>hola mundo</h1>
+  return (
+    <>
+      <h1>hola mundo</h1>
+      <Informacion/>
+    </>
+  );
 }
-export default App
+export default App;
