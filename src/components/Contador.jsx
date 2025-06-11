a2549711d61368fb979cb4b510f524320c62b4c8
Nacho <barrojojoaquin2@gmail.com>
2025-06-11 19:21:58 -0300

    funcion para restar el contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,33 +1,38 @@
 import { useState } from "react";
 
-useState
+useState;
 const Contador = () => {
-    // craer un estado
-    const [numero, setNumero] = useState(1)
-    // let numero = 1;
+  // craer un estado
+  const [numero, setNumero] = useState(1);
+  // let numero = 1;
 
-    const sumar = () => {
-        setNumero(numero + 1)
-        console.log(numero);
+  const sumar = () => {
+    setNumero(numero + 1);
+    console.log(numero);
+  };
 
+  const restar = () => {
+    if (numero <= 1) {
+      // alert("El numero no puede ser menor a 1")
+      return;
     }
+    setNumero(numero - 1);
+    console.log(numero);
+  };
 
-    const restar = () => {
-        if (numero <= 1) {
-            // alert("El numero no puede ser menor a 1")
-            return;
-        }
-        setNumero(numero - 1)
-        console.log(numero);
-    }
-    return (
-        <section className="text-center mt-5">
-            <h2>Contador</h2>
-            <h3>N°:{numero}</h3>
-            <button className="btn btn-primary me-3" onClick={sumar}>+</button>
-            <button className="btn btn-danger" onClick={restar}>-</button>
-        </section>
-    );
+  return (
+    <section className="text-center mt-5">
+      <h2>Contador</h2>
+      <h3>N°:{numero}</h3>
+      <button className="btn btn-primary me-3" onClick={sumar}>
+        +
+      </button>
+
+      <button className="btn btn-danger me-3" onClick={restar}>
+        -
+      </button>
+    </section>
+  );
 };
 
 export default Contador;
